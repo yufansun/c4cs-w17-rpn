2ce999de88dc3ec85f192336d06911cb0ac916db
yufansun <yufansun@umich.edu>
2017-03-18 12:29:55 -0400

    added color print

--- rpn.py
+++ rpn.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import operator
+import readline
+from termcolor import cprint
+import types
 
 OPERATORS = {
 	'+': operator.add,
@@ -14,6 +17,29 @@ OPERATORS = {
 def hello():
 	print("Hello world")
 
+
+def color_print(operand):
+	if type(operand) is float:
+		if operand < 0:
+			cprint(operand, 'red', attrs=['bold'], end=" ")
+		elif operand == 0:
+			cprint(operand, 'green', end=" ")
+		else:
+			cprint(operand, 'yellow', end=" ")
+	else:
+		cprint(operand, 'blue', end=" ")
+
+def color_print2(operand):
+	if type(operand) is float:
+		if operand < 0:
+			cprint(operand, 'red', attrs=['bold'])
+		elif operand == 0:
+			cprint(operand, 'green')
+		else:
+			cprint(operand, 'yellow')
+	else:
+		cprint(operand, 'blue')
+
 def calculate(arg):
 	stack = list()
 	for operand in arg.split():
@@ -25,15 +51,15 @@ def calculate(arg):
 			arg1 = stack.pop()
 			operator_fn = OPERATORS[operand]
 			result = operator_fn(arg1, arg2)
-			
 			stack.append(result)
+		color_print(operand)
 	return stack.pop()
 
 def main():
 	while True:
 		result = calculate(input('rpn calc> '))
-		hello()
-		print("Result:", result)
+		print("=",end=" ")
+		color_print2(result)
 
 if __name__ == '__main__':
 	main()
